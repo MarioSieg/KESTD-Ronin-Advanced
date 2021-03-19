55104cf634a43bfd95a50d70ebf049cc0613832e
msi <support@kerbogames.com>
2021-03-19 21:32:26 +0100

    Pipeline cleanup

--- src/impls/graphics/mod.rs
+++ src/impls/graphics/mod.rs
@@ -40,28 +40,12 @@ impl<'a> Frame<'a> {
     }
 }
 
-pub struct ShaderBundle {
+pub struct ShaderPipeline {
     pub vs_module: ShaderModule,
     pub fs_module: ShaderModule,
     pub fs_targets: SmallVec<[ColorTargetState; 8]>,
-}
-
-impl ShaderBundle {
-    pub fn vertex_state(&self) -> VertexState {
-        VertexState {
-            module: &self.vs_module,
-            entry_point: SHADER_ENTRY,
-            buffers: &[],
-        }
-    }
-
-    pub fn fragment_state(&self) -> FragmentState {
-        FragmentState {
-            module: &self.fs_module,
-            entry_point: SHADER_ENTRY,
-            targets: &self.fs_targets[..],
-        }
-    }
+    pub pipeline_layout: PipelineLayout,
+    pub render_pipeline: RenderPipeline,
 }
 
 pub const SHADER_ENTRY: &str = "main";
@@ -84,18 +68,67 @@ macro_rules! load_shader {
     };
 }
 
+pub struct ShaderPipelineDescriptor<'a> {
+    pub modules: &'a (ShaderModuleDescriptor<'a>, ShaderModuleDescriptor<'a>),
+    pub bind_group_layouts: &'a [&'a BindGroupLayout],
+    pub push_constant_ranges: &'a [PushConstantRange],
+    pub primitive_state: PrimitiveState,
+    pub depth_stencil: Option<DepthStencilState>,
+    pub multisample: MultisampleState,
+}
+
+impl<'a> ShaderPipelineDescriptor<'a> {
+    pub fn new_simple(
+        modules: &'a (ShaderModuleDescriptor<'a>, ShaderModuleDescriptor<'a>),
+    ) -> Self {
+        Self {
+            modules,
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+            primitive_state: PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: MultisampleState::default(),
+        }
+    }
+}
+
 impl Drivers {
-    pub fn load_shader_bundle(
-        &self,
-        modules: &(ShaderModuleDescriptor, ShaderModuleDescriptor),
-    ) -> ShaderBundle {
-        let vs_module = self.device.create_shader_module(&modules.0);
-        let fs_module = self.device.create_shader_module(&modules.1);
+    pub fn create_shader_bundle(&self, desc: ShaderPipelineDescriptor) -> ShaderPipeline {
+        let vs_module = self.device.create_shader_module(&desc.modules.0);
+        let fs_module = self.device.create_shader_module(&desc.modules.1);
         let fs_targets = smallvec![self.swap_chain_format.into()];
-        ShaderBundle {
+        let pipeline_layout = self
+            .device
+            .create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: desc.bind_group_layouts,
+                push_constant_ranges: desc.push_constant_ranges,
+            });
+        let render_pipeline = self
+            .device
+            .create_render_pipeline(&RenderPipelineDescriptor {
+                label: None,
+                layout: Some(&pipeline_layout),
+                vertex: VertexState {
+                    module: &vs_module,
+                    entry_point: SHADER_ENTRY,
+                    buffers: &[],
+                },
+                fragment: Some(FragmentState {
+                    module: &fs_module,
+                    entry_point: SHADER_ENTRY,
+                    targets: &fs_targets[..],
+                }),
+                primitive: desc.primitive_state,
+                depth_stencil: desc.depth_stencil,
+                multisample: desc.multisample,
+            });
+        ShaderPipeline {
             vs_module,
             fs_module,
             fs_targets,
+            pipeline_layout,
+            render_pipeline,
         }
     }
 
--- src/systems/graphics.rs
+++ src/systems/graphics.rs
@@ -1,11 +1,11 @@
 use super::System;
 use crate::config::CoreConfig;
-use crate::impls::graphics::Drivers;
+use crate::impls::graphics::{Drivers, ShaderPipeline, ShaderPipelineDescriptor};
 use crate::load_shader;
 
 pub struct GraphicsSystem {
     drivers: Drivers,
-    render_pipeline: wgpu::RenderPipeline,
+    lambert_pipeline: ShaderPipeline,
 }
 
 impl System for GraphicsSystem {
@@ -17,33 +17,13 @@ impl System for GraphicsSystem {
 
         let drivers = Drivers::initialize(window, is_power_safe_mode, use_vsync);
 
-        let lambert = drivers.load_shader_bundle(load_shader!("lambert"));
-
-        let pipeline_layout =
-            drivers
-                .device
-                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                    label: None,
-                    bind_group_layouts: &[],
-                    push_constant_ranges: &[],
-                });
-
-        let render_pipeline =
-            drivers
-                .device
-                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                    label: None,
-                    layout: Some(&pipeline_layout),
-                    vertex: lambert.vertex_state(),
-                    fragment: Some(lambert.fragment_state()),
-                    primitive: wgpu::PrimitiveState::default(),
-                    depth_stencil: None,
-                    multisample: wgpu::MultisampleState::default(),
-                });
+        let lambert_pipeline = drivers.create_shader_bundle(ShaderPipelineDescriptor::new_simple(
+            load_shader!("lambert"),
+        ));
 
         Self {
             drivers,
-            render_pipeline,
+            lambert_pipeline,
         }
     }
 
@@ -51,7 +31,7 @@ impl System for GraphicsSystem {
         let mut frame = self.drivers.begin_frame();
         {
             let mut pass = frame.create_pass();
-            pass.set_pipeline(&self.render_pipeline);
+            pass.set_pipeline(&self.lambert_pipeline.render_pipeline);
             pass.draw(0..3, 0..1);
         }
 
